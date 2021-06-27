ad3955ff94488a6252fa8547238f9417e2a04423
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-06-26 19:15:48 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,30 +5,63 @@ var d = new Date();
 var time = d.toLocaleTimeString();
 document.getElementById("time").innerHTML = time;
 }
-//open Home app
+
+//variable section
+var osLogo = "Avocado OS<br><img src='https://lb123658.github.io/os/img/logo.svg' style='width:280px;'><br>Version 1.0";
+var appName = document.getElementById("appName");
+var about = document.getElementById("about");
+var info = document.getElementById("info");
+var embed = document.getElementById("embed");
+
+//app opening functions
 function openHome() {
-var app = "Home";
 var appDescription = "<b>Home</b><br>The homepage for Avocado OS";
 var appInfo = "Home<br>Version 1.0<br>©2021 Avocado";
 var appCode = "App running: Home<br>Written in: HTML";
-document.getElementById("appName").innerHTML = "Home";
-// hide other apps
-var x = document.getElementsByTagName("EMBED");
-var i;
-for (i = 0; i < x.length; i++) {
-  x[i].style.zIndex = "-1";
+appName.innerHTML = "Home";
+about.addEventListener("click", function() {
+  notify(appInfo);
+  });
+appName.addEventListener("click", function() {
+  notify(appDescription);
+  });
+info.addEventListener("click", function() {
+  notify(appCode);
+  });
+embed.src = "https://lb123658.github.io/os/apps/home";
 }
-}
-
-//open app store
 function openStore() {
-var app = "App Store";
-var appDescription = "<b>Home</b><br>The official app store for Avocado OS";
+var appDescription = "<b>App Store</b><br>The official app store for Avocado OS";
 var appInfo = "App Store<br>Version 1.0<br>©2021 Avocado";
 var appCode = "App running: App Store<br>Written in: HTML";
-document.getElementById("appName").innerHTML = "App Store";
+appName.innerHTML = "App Store";
+about.addEventListener("click", function() {
+  notify(appInfo);
+  });
+appName.addEventListener("click", function() {
+  notify(appDescription);
+  });
+info.addEventListener("click", function() {
+  notify(appCode);
+  });
+embed.src = "https://lb123658.github.io/os/apps/store";
 }
-
+function openBrowser() {
+var appDescription = "<b>Browser</b><br>The browser for Avocado OS";
+var appInfo = "Avocado Browser<br>Version 1.0<br>©2021 Avocado";
+var appCode = "App running: Avocado Browser<br>Written in: HTML";
+appName.innerHTML = "Browser";
+about.addEventListener("click", function() {
+  notify(appInfo);
+  });
+appName.addEventListener("click", function() {
+  notify(appDescription);
+  });
+info.addEventListener("click", function() {
+  notify(appCode);
+  });
+embed.src = "https://lb123658.github.io/os/apps/browser";
+}
 
 function loadMain() {
 document.getElementById("load").style.visibility = "hidden";
@@ -48,4 +81,15 @@ element.style.visibility = "visible";
 }
 function hide(element) {
 element.style.visibility = "hidden";
+}
+
+function openFullscreen() {
+var elem = document.getElementById("app");
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { // Safari
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { // IE11
+    elem.msRequestFullscreen();
+  }
 }
